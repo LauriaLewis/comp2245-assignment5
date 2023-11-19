844c7f17ea6a0ac439d7cf7bb0c8db1580eb912e
Lauria MK Lewis <lauria.lewis@my.fiveislands.uwi.edu>
2023-11-18 22:04:23 -0400

    corrected from ajax used in assignment 4

--- world.js
+++ world.js
@@ -1,24 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  //get the look up button
-  const lookupBtn = document.getElementById("lookup");
+document.addEventListener('DOMContentLoaded', function(){
+    // Get the lookup button and lookup cities button
+    const lookupBtn = document.getElementById('lookup');
+    const lookupCitiesBtn = document.getElementById('lookup-cities');
 
-  lookupBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    const country = document.getElementById("country");
-    const result = document.getElementById("result");
-    let requested = country.name + "=" + country.value;
-    console.log(requested);
+    // Function to handle the XMLHttpRequest
+    function makeRequest(url, callback) {
+        return function (event) {
+            event.preventDefault();
 
-    const xhr = new XMLHttpRequest();
+            const country = document.getElementById('country');
+            const result = document.getElementById('result');
+            let requested = country.name + "=" + country.value;
 
-    // when we receive the contents from the server
-    xhr.onload = function () {
-      console.log(this.responseText);
-      result.innerHTML = this.responseText;
-    };
+            const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "world.php?" + requested, true);
-    // xhr.setRequestHeader("content-type", 'application/x-www-form-urlencoded');
-    xhr.send();
-  });
+            xhr.onload = function () {
+                callback(result, this.responseText);
+            }
+
+            xhr.open("GET", url + requested, true);
+            xhr.send();
+        };
+    }
+
+    // Add click event listeners for both buttons
+    lookupBtn.addEventListener('click', makeRequest("world.php?", updateResult));
+    lookupCitiesBtn.addEventListener('click', makeRequest("world.php?context=cities&", updateResult));
+
+    // Callback function to update the result element
+    function updateResult(resultElement, responseText) {
+        console.log(responseText);
+        resultElement.innerHTML = responseText;
+    }
 });
