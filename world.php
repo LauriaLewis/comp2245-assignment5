0451899391e68d468d04bfa8fc6e2760c67483cd
Lauria MK Lewis <lauria.lewis@my.fiveislands.uwi.edu>
2023-11-18 21:17:13 -0400

    Updated changes

--- world.php
+++ world.php
@@ -1,17 +1,27 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+$countryParam = isset($_GET["country"]) ? trim($_GET["country"]) : null;
+$filteredCountry = filter_var($countryParam, FILTER_SANITIZE_STRING);
+
+$query = "SELECT * FROM countries";
+
+if ($filteredCountry) {
+    $query .= " WHERE name LIKE '%$filteredCountry%'";
+}
+
+$stmt = $conn->query($query);
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+    <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
